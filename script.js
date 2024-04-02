e4aa2df823da4f89eba66793dc853d8564360e4f
KAuser2094 <kauser2094@gmail.com>
2024-04-02 13:04:00 +0100

    Fixed colours to match better

--- script.js
+++ script.js
@@ -12,68 +12,45 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
   var cardEffect = document.getElementById("cardEffect").value;
 
   const CARD_WIDTH = 300;
-  const CARD_HEIGHT = 400;
+  const CARD_HEIGHT = 410;
+
+  const weirdDarkGrey = "#3C3D3C";
+  const weirdBlue = "#446390";
+  const weirdPurple = "#662F76";
+
+  const COLOURS = {
+  Basic: weirdDarkGrey,
+  Common: "white",
+  Rare: weirdBlue,
+  Epic: weirdPurple,
+  Legendary: "#C78212",
+  Darkness: weirdPurple,
+  Nature: "green",
+  Fire: "#983232",
+  Water: weirdBlue,
+  Light: "#EDDB3C",
+  Neutral: weirdDarkGrey,
+  TextBox: weirdDarkGrey,
+  };
 
   // Determine color and text based on card class
-  var classColor;
-  switch (cardClass) {
-    case "Darkness":
-      classColor = "purple";
-      break;
-    case "Nature":
-      classColor = "green";
-      break;
-    case "Fire":
-      classColor = "red";
-      break;
-    case "Water":
-      classColor = "blue";
-      break;
-    case "Light":
-      classColor = "yellow";
-      break;
-    case "Neutral":
-      classColor = "darkgrey";
-      break;
-    default:
-      classColor = "black"; // Default color
-  }
+  var classColour = COLOURS[cardClass] || "brown";
 
   // Determine color based on card rarity
-  var rarityColor;
-  switch (cardRarity) {
-    case "Common":
-      rarityColor = "#f2f2f2"; // Very light grey
-      break;
-    case "Rare":
-      rarityColor = "blue"; // Same blue as Water
-      break;
-    case "Epic":
-      rarityColor = "purple"; // Same purple as Darkness
-      break;
-    case "Legendary":
-      rarityColor = "orange";
-      break;
-    default:
-      rarityColor = "black"; // Default color
-  }
+  var rarityColour = COLOURS[cardRarity] || "brown";
 
   // Load image into Fabric.js
   var canvas = new fabric.Canvas('generatedCanvas', { width: CARD_WIDTH, height: CARD_HEIGHT });
-  fabric.Image.fromURL(URL.createObjectURL(cardImage), function(img) {
-    var scaleX = CARD_WIDTH / img.width;
-    var scaleY = CARD_HEIGHT / img.height;
-    var scale = Math.min(scaleX, scaleY);
-
-    // Scale the image proportionally
-    img.scale(scale);
-
-    // Yes the scaling doesn't work -_-
-    
+  canvas.clear();
+  fabric.Image.fromURL(URL.createObjectURL(cardImage), function(img) {    
     // Position the image at the center of the canvas
     img.set({
       left: 0,
-      top: 0
+      top: 0,
+    });
+
+    img.set({
+      objectCorners: 10 // Set the radius of the rounded corners
     });
 
     // Add the image to the canvas
@@ -84,21 +61,22 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
       left: 0,
       top: 0,
       width: 45,
-      height: 90,
-      fill: classColor,
+      height: 80,
+      fill: classColour,
       originX: 'left',
       originY: 'top',
       rx: 10, // Rounded corners
       ry: 10, // Rounded corners
-      opacity: 0.7 // Translucency
+      opacity: 0.7, // Translucency
     });
     var classText = new fabric.Text(cardWeight, {
       left: classRect.left + classRect.width / 2,
       top: classRect.top + classRect.height / 2,
       fill: 'white',
-      fontSize: 30,
+      fontSize: 65,
       originX: 'center',
-      originY: 'center'
+      originY: 'center',
+      fontFamily: 'Source Sans Pro', // Apply Source Sans font to class text
     });
     var classGroup = new fabric.Group([classRect, classText], {
       left: 0,
@@ -111,13 +89,13 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
       left: 0,
       top: 0,
       width: CARD_WIDTH - classRect.width, // Remaining width of the card
-      height: 35,
-      fill: rarityColor, // Use rarity color
+      height: 33,
+      fill: rarityColour, // Use rarity color
       originX: 'left',
       originY: 'top',
       rx: 10, // Rounded corners
       ry: 10, // Rounded corners
-      opacity: 0.7 // Translucency
+      opacity: 0.7, // Translucency
     });
     var nameText = new fabric.Text(cardName, {
       left: nameRect.left + nameRect.width / 2,
@@ -125,7 +103,8 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
       fill: 'white',
       fontSize: 25,
       originX: 'center',
-      originY: 'center'
+      originY: 'center',
+      fontFamily: 'Source Sans Pro', // Apply Source Sans font to class text
     });
     var nameGroup = new fabric.Group([nameRect, nameText], {
       left: classRect.width,
@@ -139,13 +118,13 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
         left: classRect.width,
         top: nameRect.height,
         width: CARD_WIDTH - classRect.width,
-        height: 35,
-        fill: 'darkgrey',
+        height: 33,
+        fill: COLOURS.TextBox,
         originX: 'left',
         originY: 'top',
         rx: 10,
         ry: 10,
-        opacity: 0.7
+        opacity: 0.7,
       });
       var groupText = new fabric.Text(cardGroup, {
         left: groupRect.left + groupRect.width / 2,
@@ -153,7 +132,8 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
         fill: 'white',
         fontSize: 25,
         originX: 'center',
-        originY: 'center'
+        originY: 'center',
+        fontFamily: 'Source Sans Pro', // Apply Source Sans font to class text
       });
       var groupGroup = new fabric.Group([groupRect, groupText], {
         left: classRect.width,
@@ -165,18 +145,18 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
     var currentBottom = 0;
     // Add a rectangle for the card power if not empty
     if (cardPower) {
-      const POWER_RECT_HEIGHT = 70
+      const POWER_RECT_HEIGHT = 85
       var powerRect = new fabric.Rect({
         left: 0,
         top: CARD_HEIGHT - POWER_RECT_HEIGHT,
         width: CARD_WIDTH,
         height: POWER_RECT_HEIGHT,
-        fill: 'darkgrey',
+        fill: COLOURS.TextBox,
         originX: 'left',
         originY: 'top',
         rx: 10,
         ry: 10,
-        opacity: 0.7
+        opacity: 0.7,
       });
       var powerText = new fabric.Text(cardPower, {
         left: powerRect.left + powerRect.width / 2,
@@ -186,13 +166,14 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
         originX: 'center',
         originY: 'center',
         // fontWeight: 'bold'
+        fontFamily: 'Source Sans Pro', // Apply Source Sans font to class text
       });
       var powerGroup = new fabric.Group([powerRect, powerText], {
         left: 0,
         top: CARD_HEIGHT - POWER_RECT_HEIGHT
       });
       canvas.add(powerGroup);
-      currentBottom += POWER_RECT_HEIGHT + 5;
+      currentBottom += POWER_RECT_HEIGHT + 1;
     }
 
     // Add a rectangle for the card effect if not empty
@@ -204,12 +185,12 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
         top: 0,
         width: CARD_WIDTH,
         height: lines * heightPerLine,
-        fill: 'darkgrey',
+        fill: COLOURS.TextBox,
         originX: 'left',
         originY: 'top',
         rx: 10,
         ry: 10,
-        opacity: 0.7
+        opacity: 0.7,
       });
       var effectText = new fabric.Text(cardEffect, {
         left: effectRect.left + effectRect.width / 2,
@@ -218,7 +199,7 @@ document.getElementById("cardForm").addEventListener("submit", function (event) 
         fontSize: 25,
         originX: 'center',
         originY: 'center',
-        fontWeight: 'bold'
+        fontFamily: 'Source Sans Pro', // Apply Source Sans font to class text
       });
       var effectGroup = new fabric.Group([effectRect, effectText], {
         left: 0,
